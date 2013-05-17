4dba44328622cc6d666d14734dfbe02a28281598
Parnell Springmeyer <ixmatus@gmail.com>
2013-05-17 09:44:50 -0500

    Adding a test for email validator

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,4 +1,6 @@
-from colander_validators import url
+from colander_validators import (
+    email,
+    url)
 
 
 def test_url():
@@ -7,3 +9,11 @@ def test_url():
     assert url("http://bleh.net") == True
     assert type(url("://ixmat.us")) == str
     assert type(url("ixmat")) == str
+
+
+def test_email():
+
+    assert email("barney@purpledino.com") == True
+    assert email("barney.10.WHATDINO@purple.com") == True
+    assert type(email("barney")) == str
+    assert type(email("barney@dino")) == str
